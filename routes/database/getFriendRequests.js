5621db222b3d275aa1c6516fe977b648d4204677
Xaliks <me@xaliks.dev>
2024-02-16 18:15:14 +0300

    Move readFriendRequest to other file

--- routes/database/getFriendRequests.js
+++ routes/database/getFriendRequests.js
@@ -1,3 +1,4 @@
+const Logger = require("../../scripts/Logger");
 const { secretMiddleware, requiredBodyMiddleware } = require("../../scripts/middlewares");
 const { database } = require("../../scripts/database");
 const { toSafeBase64 } = require("../../scripts/security");
@@ -71,6 +72,41 @@ module.exports = (fastify) => {
 					where: { id: { in: friendRequests.map((friendRequest) => friendRequest.id) } },
 					data: { isNew: false },
 				});
+			}
+		},
+	});
+
+	fastify.route({
+		method: ["POST"],
+		url: "/readGJFriendRequest20.php",
+		beforeHandler: [secretMiddleware, requiredBodyMiddleware(["accountID", "gjp2", "requestID"])],
+		handler: async (req, reply) => {
+			const { accountID, gjp2, requestID } = req.body;
+
+			try {
+				const account = await database.accounts.findFirst({ where: { id: parseInt(accountID), password: gjp2 } });
+				if (!account) return reply.send("-1");
+
+				const friendRequest = await database.friendRequests
+					.update({
+						where: { id: parseInt(requestID), toAccountId: account.id },
+						data: { isNew: false },
+					})
+					.catch(() => null);
+				if (!friendRequest) return reply.send("-1");
+
+				Logger.log(
+					"Read friend request",
+					`ID: ${Logger.color(Logger.colors.cyan)(friendRequest.id)}\n`,
+					`From: ${Logger.color(Logger.colors.gray)(friendRequest.accountId)}\n`,
+					`To: ${Logger.color(Logger.colors.cyan)(account.username)}/${Logger.color(Logger.colors.gray)(friendRequest.toAccountId)}`,
+				);
+
+				return reply.send("1");
+			} catch (error) {
+				Logger.error("Read friend request", req.body, error);
+
+				return reply.send("-1");
 			}
 		},
 	});
