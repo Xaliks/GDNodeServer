39dccd6a4eba452a9b207d8c3fad95aa1cf2530b
Xaliks <me@xaliks.dev>
2024-03-31 14:45:04 +0300

    fix

--- routes/database/accounts/management.js
+++ routes/database/accounts/management.js
@@ -2,7 +2,7 @@
  * @param {import("fastify").FastifyInstance} fastify
  */
 module.exports = (fastify) => {
-	["/accountManagement.php", "/backupGJAccountNew.php", "/lostpassword.php", "lostusername.php"].forEach((url) =>
+	["/accountManagement.php", "/backupGJAccountNew.php", "/lostpassword.php", "/lostusername.php"].forEach((url) =>
 		fastify.get(url, (req, reply) => reply.redirect("/dashboard/account")),
 	);
 };
